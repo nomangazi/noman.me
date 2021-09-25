4f75c055502b6806e6ef595af92c506808153c6e
Noman Gazi <55198320+nomangazicse@users.noreply.github.com>
2021-09-25 15:00:13 +0600

    fix resume file link

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -96,7 +96,7 @@ const Header = () => {
                 className="btn-download"
                 target="_blank"
                 rel="noopener noreferrer"
-                href="https://github.com/zonayedpca/zonayed.me/raw/master/src/data/file/MyResume.pdf"
+                href="https://github.com/nomangazicse/nomangazicse.me/blob/main/src/data/file/MyResume.pdf"
               >
                 Resume
               </a>
