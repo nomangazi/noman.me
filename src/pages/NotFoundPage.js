f9209388f44944c822056f5ce5c0003de9266481
nomangazicse <numan.islam61@gmail.com>
2023-05-24 10:56:03 +0600

    updated

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,10 +1,10 @@
 import React from "react"
 
-import { Layout, SEO } from "../components/common"
+import { Layout, SeoPage } from "../components/common"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
+    <SeoPage title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>OOPS! You just hit a page that doesn&#39;t exist...</p>
   </Layout>
