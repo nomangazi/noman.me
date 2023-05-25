eb90cc514ba56631f5acc212d9ab1b3ff964b645
Noman Gazi <numan.islam61@gmail.com>
2023-05-25 11:46:41 +0600

    Revert "updated"
    This reverts commit f9209388f44944c822056f5ce5c0003de9266481.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,10 @@
 import React from "react"
 
-import { Layout, SeoPage } from "../components/common"
+import { Layout, SEO } from "../components/common"
 
 const NotFoundPage = () => (
   <Layout>
-    <SeoPage title="404: Not found" />
+    <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>OOPS! You just hit a page that doesn&#39;t exist...</p>
   </Layout>
