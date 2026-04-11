f5f57e5c09272aa88837703186fc66047612f09e
Noman Gazi <nomangazix@gmail.com>
2026-04-11 22:59:44 +0600

    Remove Gatsby portfolio site
    Delete the entire Gatsby-based portfolio project and related files. Removes source (src/), components, assets and data, Gatsby config/entry files (gatsby-config.js, gatsby-node.js, gatsby-browser.js, gatsby-ssr.js), project manifest and docs (package.json, README, LICENSE), CI config, static site files and other project artifacts to prepare the repository for new content.

--- src/components/common/logo.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import logo from "../../assets/image/icon.png"
-
-export const Logo = () => {
-  return (
-    <img src={logo} className="logo" alt="Logo"/>
-  )
-}
--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export * from "./Intro"
-export * from "./About"
-export * from "./Skill"
-export * from "./LoveToDo"
-export * from "./Portfolio"
-export * from "./Contact"
-
-// Path: src/components/common/index.js
